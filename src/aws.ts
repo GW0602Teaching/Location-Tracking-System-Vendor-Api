65a3ff86825b6209ae9476f50908825c3f1ae527
xuyang0602 <wang.xuyang0602@gmail.com>
2023-09-26 16:39:48 -0700

    Add message removal method

--- src/aws.ts
+++ src/aws.ts
@@ -167,12 +167,46 @@ export const dynamoDbRemoveConnection = async (
   }
 };
 
-const execRemoveConnection = async () => {
-  const res = await dynamoDbRemoveConnection(
-    'websocket-connections',
-    '123'
+// const execRemoveConnection = async () => {
+//   const res = await dynamoDbRemoveConnection(
+//     'websocket-connections',
+//     '123'
+//   );
+//   console.log(res);
+// };
+
+// execRemoveConnection();
+
+export const sqsDeleteMessage = async (
+  queueUrl: string,
+  receiptHandle: string
+) => {
+  try {
+    const params: AWS.SQS.DeleteMessageRequest = {
+      ReceiptHandle: receiptHandle,
+      QueueUrl: queueUrl,
+    };
+
+    const res = await sqs.deleteMessage(params).promise();
+    console.log('Message deleted!');
+    return res;
+  } catch (e) {
+    if (e instanceof Error) {
+      return e;
+    }
+
+    return new Error(`sqsDeleteMessage error object unknown type`);
+  }
+};
+
+const execSqsDeleteMessage = async () => {
+  const receiptHandle =
+    'AQEBo8WLWoDJdh0kFyg6bzezPWH7ND1c6ND1BBbGwObaQXsbj9u2f989XZc4lvn3NAu3YylIaTBmdV3mDRMsc+nJ4LKg4Rq67/zsiwFbhTcnKlpaN47EUE7+Jk44F1tnlFvotgWRPkXiAHf6rMqP+g+fQK4GWyUktOkydkEj6etqJcQajDhsiUtMTTINF/XgYLbN8NjGMtFE9O+C3SLFqejadMlhOI/NnlPLES0VVDAQ+dQoCpbgnQIqMsef7GTBDZbJ8nF16uzDWrh67YQVRgltEB5+WWCLDZVgcFTqHr59a16gwzD8UV4sOEha7R7YDVNRt+IqxZ5f6z4CCDt8TsCP8D8i8oOctgcjHI++PyQ8Fsh6yiQ6KI8SJUtvuxSlGzXr6XVmCSO49yBzw8LCnUdZCQ==';
+  const res = await sqsDeleteMessage(
+    'https://sqs.us-east-1.amazonaws.com/525480118775/vender-twitter-queue',
+    receiptHandle
   );
   console.log(res);
 };
 
-execRemoveConnection();
+execSqsDeleteMessage();
